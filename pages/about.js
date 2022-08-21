0ac99e50f8807a59c759e0102c7356104cb164f2
bayupb <kampuspb.bayu@gmail.com>
2022-08-21 22:00:29 +0700

    NextJs Guys

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import Header from '../components/header.js'
+
+function About() {
+
+  return (
+    <div className='bg-blue-500 px-5 py-2'>
+      <div className='flex max-w-5xl mx-auto justify-between items-center'>
+        <div>Logo</div>
+        <div>Logo</div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default About
