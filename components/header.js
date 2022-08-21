4008074df7162b818fc4de4ae527e0b1a8e67fcc
bayupb <kampuspb.bayu@gmail.com>
2022-08-21 22:13:51 +0700

    header js

--- components/header.js
+++ components/header.js
@@ -1,9 +1,9 @@
 function Header() {
   return (
-    <div className='bg-blue-200 px-5 py-2'>
+    <div className='bg-black px-5 py-4'>
       <div className='flex max-w-3xl mx-auto justify-between items-center'>
         <div className='text-white text-2xl uppercase'>NEXTJS</div>
-        <div className='bg-blue-500 px-5 py-2 text-white rounded-md cursor-pointer'>Download NextJS</div>
+        <div className='bg-blue-500 text-sm px-5 py-2 text-white rounded-md cursor-pointer'>Download NextJS</div>
         
       </div>
     </div>
