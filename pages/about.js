ba113360dfb8349b0f02189653c84952a23d2d57
bayupb <kampuspb.bayu@gmail.com>
2022-08-21 22:11:33 +0700

    pembaharuan components

--- pages/about.js
+++ pages/about.js
@@ -3,13 +3,7 @@ import Header from '../components/header.js'
 function About() {
 
   return (
-    <div className='bg-blue-500 px-5 py-2'>
-      <div className='flex max-w-5xl mx-auto justify-between items-center'>
-        <div>Logo</div>
-        <div>Logo</div>
-        
-      </div>
-    </div>
+    <Header/>
   )
 }
 
