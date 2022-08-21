7c689899e66330e8a33dd12b26b74dd5dc513cbd
bayupb <kampuspb.bayu@gmail.com>
2022-08-21 22:22:58 +0700

    added head title anda change index.js

--- components/header.js
+++ components/header.js
@@ -1,12 +1,20 @@
+import Link from 'next/link'
+
 function Header() {
   return (
+    <>
     <div className='bg-black px-5 py-4'>
-      <div className='flex max-w-3xl mx-auto justify-between items-center'>
-        <div className='text-white text-2xl uppercase'>NEXTJS</div>
-        <div className='bg-blue-500 text-sm px-5 py-2 text-white rounded-md cursor-pointer'>Download NextJS</div>
+      <div className='flex max-w-xl mx-auto justify-between items-center'>
+        <div className='text-white text-2xl uppercase'>
+          <Link href="/">
+            <a>BIO</a>
+          </Link>
+        </div>
+        <div className='bg-blue-500 text-sm px-5 py-1 text-white rounded-md cursor-pointer'>Download NextJS</div>
         
       </div>
     </div>
+    </>
   )
 }
 
